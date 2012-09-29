acf8b4083cfdb3441a7f3c957ec136c11fef04ec
David Kormushoff <kormie@gmail.com>
2012-09-29 16:25:03 -0500

    Add edit, index and new actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,38 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    if @restaurant.save
+      redirect_to restaurants_url
+    else
+      render action: "new"
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
+
+
+
+  def update
+
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(params[:restaurant])
+      redirect_to restaurants_path
+    else
+      render action: "edit"
+    end
+  end
 end
